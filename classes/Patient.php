eea53fef3aa05bd1f8743c3e47b4e5bd31d67204
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-12-24 17:10:51 +0100

    created class Patient

---
+++ classes/Patient.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__.'/Utilisateur.php';
+
+class Patient extends Utilisateur{
+    private $rendez_vous;
+
+    public function __construct($nom, $prenom, $type_utilisateur, $rendez_vous){
+        parent::__construct($nom, $prenom, $type_utilisateur);
+        $this->rendez_vous;
+    }
+
+    public function prendreRendezVous($rendez_vous){
+        $this->rendez_vous = $rendez_vous;
+    }
+
+    public function __toString(){
+        return $this->nom.' - '. $this->prenom.' - '.$this->type_utilisateur.' - '.$this->rendez_vous;
+    }
+}
