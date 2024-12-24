4b30d08902853ece11d854c9235f10ee2297317b
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-12-24 17:14:05 +0100

    fin.

--- classes/Patient.php
+++ classes/Patient.php
@@ -6,7 +6,7 @@ class Patient extends Utilisateur{
 
     public function __construct($nom, $prenom, $type_utilisateur, $rendez_vous){
         parent::__construct($nom, $prenom, $type_utilisateur);
-        $this->rendez_vous;
+        $this->rendez_vous = $rendez_vous;
     }
 
     public function prendreRendezVous($rendez_vous){
